456735b918e37c62e0257b884a8434a524044297
Spydirwebb <webb.jeffery.89@gmail.com>
2021-05-27 23:02:38 +0000

    particles_created_not_implemented

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -4,13 +4,37 @@ import Sidebar from './Components/Sidebar'
 
 function App() {
   return (
-    <AppStyled>
+    <div className="App">
       <Sidebar />
-    </AppStyled>
+      <MainContentStyled>
+          <div className="lines">
+              <div className="line-1"></div>
+              <div className="line-2"></div>
+              <div className="line-3"></div>
+              <div className="line-4"></div>
+          </div>
+      </MainContentStyled>
+    </div>
   );
 }
 
-const AppStyled = styled.div`
-    background-color: red;
+const MainContentStyled = styled.main`
+    postiion: relative;
+    margin-left: 16.3rem;
+    min-height: 100vh;
+
+    .lines{
+        position: absolute;
+        min-height: 100vh;
+        width: 100%;
+        display: flex;
+        justify-content: space-evenly;
+
+        .line-1, .line-2, .line-3, .line-4{
+            width: 1px;
+            min-height: 100vh;
+            background-color: var(--border-color);
+        }
+    }
 `;
 export default App;
---
+++ portfolio/src/Pages/HomePage.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const HomePage = () => {
+    return(
+        <HomePageStyled>
+            
+        </HomePageStyled>
+    )
+
+}
+
+const HomePageStyled = styled.header`
+    width: 100%;
+    height: 100vh;
+
+`;
+
+export default HomePage;
