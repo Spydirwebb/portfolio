b67449b27e8ecdd6805896d77685cb30f2735636
Spydirwebb <webb.jeffery.89@gmail.com>
2021-07-01 00:03:20 +0000

    portfolio page updated

--- portfolio/src/Components/Menu.js
+++ portfolio/src/Components/Menu.js
@@ -38,11 +38,12 @@ const MenuItemStyled = styled.div`
     display: grid;
     grid-template-columns: repeat(3, 1fr);
     grid-gap: 2rem;
+   
     .grid-item{
-        margin-bottom: 2rem;
         .portfolio-content{
             display: block;
             position: relative;
+            overflow: hidden;
             h6{
                 font-size: 1.5rem;
             }
@@ -52,21 +53,81 @@ const MenuItemStyled = styled.div`
                 object-fit: cover;
             }
             ul{
-                display: none;
+                transform: translateY(-600px);
+                transition: all .4s ease-in-out;
+                position: absolute;
+                left: 50%;
+                top: 40%;
+                opacity: 0;
+                li{
+                        background-color: var(--border-color);
+                        display: flex;
+                        align-items: center;
+                        justify-content: center;
+                        padding: 1rem;
+                        border-radius: 50%;
+                        width: 3rem;
+                        height: 3rem;
+                        margin: 0 .5rem;
+                        transition: all .4s ease-in-out;
+                        &:hover{
+                            background-color: var(--primary-color);
+                        }
+                        a{
+                            display: flex;
+                            align-items: center;
+                            justify-content: center;
+                            transition: all .4s ease-in-out;
+                        }
+                    }
             }
-            
             .portfolio-image{
                 &::before{
                     content: "";
                     position: absolute;
-                    left: 15px;
-                    top: 15px;
-                    height: calc(100% - 30px);
-                    width: calc(100% - 30px);
+                    left: 2%;
+                    top: 4%;
+                    height: 0;
+                    width: 0;
+                    transition: all .4s ease-in-out;
+                }
+            }
+            .portfolio-image:hover{
+                ul{
+                    transform: translateY(0);
+                    transform: translate(-50%, -50%);
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    transition: all .4s ease-in-out;
+                    opacity: 1;
+                    li{
+                        transition: all .4s ease-in-out;
+                        &:hover{
+                            background-color: var(--primary-color);
+                        }
+                        a{
+                            display: flex;
+                            align-items: center;
+                            justify-content: center;
+                            transition: all .4s ease-in-out;
+                        }
+                    }
+                    li:hover{
+                        svg{
+                            color: var(--white-color);
+                        }
+                    }
+                    svg{
+                        font-size: 2rem;
+                    }
+                }
+                &::before{
+                    height: calc(100% - 32%) ;
+                    width: calc(100% - 4%);
                     background-color: white;
                     opacity: 0.9;
-                    tranform-origin: 0;
-                    transform: scale(1);
+                    transform-origin: left;
                     transition: all .4s ease-in-out;
                 }
             }
