2cfb2f721fc50e8de1172b72bb2a85a116974861
Spydirwebb <webb.jeffery.89@gmail.com>
2021-08-20 05:46:23 +0000

    removed scrolling nav

--- portfolio/src/Components/Navigation.js
+++ portfolio/src/Components/Navigation.js
@@ -96,9 +96,9 @@ const NavigationStyled = styled.nav`
     height: 100%;
     width: 100%;
     border-right: 1px solid var(--border-color);
-    @media screen and (max-height: 645px){
-        overflow: scroll;
-    }
+    //@media screen and (max-height: 645px){
+        //overflow: scroll;
+    //}
     .avatar{
         width: 100%;
         border-bottom: 1px solid var(--border-color);
